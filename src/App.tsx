6faab3de4675790cdaa423fbc246bd2517567a76
Usman Khalid <65516140+Rajputusman@users.noreply.github.com>
2021-08-28 00:54:20 +0500

    features(updates): Improved the app and added few new features

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import DUMMY_NOTES from "./DUMMY_NOTES";
+import {
+  deleteNote,
+  createNote,
+  getNotes,
+  updateNote,
+} from "./services/notesService";
+// import DUMMY_NOTES from "./DUMMY_NOTES";
 import Note from "./components/Note/Note";
 import INote from "./interfaces/note.interface";
+import { Button, FloatingLabel, Form, Modal } from "react-bootstrap";
 
 function App() {
   // const getNotes = () =>{
@@ -11,24 +18,46 @@ function App() {
   // let notesList: any[] = []; //note list variable
 
   const [notesList, setNotesList] = useState<Array<INote>>([]);
+  const [ShowAddModal, setShowAddModal] = useState(false);
+  const handleCloseAddModal = () => {
+    setNewNote({
+      link: "",
+      text: "",
+    });
+    setShowAddModal(false);
+  };
+  const handleShowAddModal = () => setShowAddModal(true);
+
+  const [newNote, setNewNote] = useState<Partial<INote>>({
+    link: "",
+    text: "",
+  });
   // const [notesList, setNotesList] = useState<any[]>([]);
 
-  // App components renders first time
+  // App components renders first time get items from local storage
+  // useEffect(() => {
+  //   const listFromStorageString = localStorage.getItem("my-notes");
+  //   if (listFromStorageString) {
+  //     const listFromStorageArray = JSON.parse(listFromStorageString);
+  //     setNotesList(listFromStorageArray);
+  //   } else {
+  //     setNotesList(DUMMY_NOTES);
+  //   }
+  // }, []);
   useEffect(() => {
-    const listFromStorageString = localStorage.getItem("my-notes");
-    if (listFromStorageString) {
-      const listFromStorageArray = JSON.parse(listFromStorageString);
-      setNotesList(listFromStorageArray);
-    } else {
-      setNotesList(DUMMY_NOTES);
-    }
+    getNotesFromServer();
   }, []);
 
-  useEffect(() => {
-    console.log("saving to localstorage");
-    const notesListString = JSON.stringify(notesList);
-    localStorage.setItem("my-notes", notesListString);
-  }, [notesList]);
+  const getNotesFromServer = async () => {
+    const notes = await getNotes();
+    setNotesList(notes);
+  };
+
+  // useEffect(() => {
+  //   console.log("saving to localstorage");
+  //   const notesListString = JSON.stringify(notesList);
+  //   localStorage.setItem("my-notes", notesListString);
+  // }, [notesList]);
 
   // useEffect(() => {
   //   console.log("noteList list value changed");
@@ -45,31 +74,99 @@ function App() {
   //     console.error(error);
   //   }
   // }
-  console.log("rerendering");
-  console.log(notesList);
-
-  const updateNoteItem = (updatedNote: INote) => {
-    console.log("value updated in the app component");
-    console.log(updatedNote);
-
+  // console.log("rerendering");
+  // console.log(notesList);
+  const updateNoteItem = async (updatedNote: INote) => {
+    const noteFromServer = await updateNote(updatedNote);
     // temporary variable
     const updatedList = notesList.map((noteItem: INote) => {
-      if (noteItem._id === updatedNote._id) {
-        return updatedNote;
+      if (noteItem._id === noteFromServer._id) {
+        return noteFromServer;
       }
       return noteItem;
     });
     setNotesList(updatedList); // updating the state of notes list
   };
 
+  const addNote = async () => {
+    const savedNote = await createNote(newNote);
+    setNotesList([...notesList, savedNote]);
+    handleShowAddModal();
+  };
+  const deleteNoteItem = async (noteToDelete: INote) => {
+    await deleteNote(noteToDelete._id);
+    const remainingNotes = notesList.filter((noteItem) => {
+      return noteItem._id !== noteToDelete._id;
+    });
+    setNotesList(remainingNotes);
+  };
+
   return (
     <div className="App">
       <div className="t1-main">Notes Application</div>
-      <div></div>
+      <Button
+        variant="dark"
+        className="add-button"
+        onClick={handleShowAddModal}
+      >
+        +
+      </Button>
+
+      <Modal show={ShowAddModal} onHide={handleCloseAddModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Note</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FloatingLabel controlId="floatingTextarea2" label="Comments">
+            <Form.Control
+              onChange={(event) => {
+                const NewVal = event.currentTarget.value;
+                setNewNote({
+                  ...newNote,
+                  text: NewVal,
+                });
+              }}
+              as="textarea"
+              placeholder="Enter your text"
+              style={{ height: "100px" }}
+            />
+          </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingTextarea"
+            label="Link"
+            className="mb-3 note-link"
+          >
+            <Form.Control
+              onChange={(event) => {
+                const NewVal = event.currentTarget.value;
+                setNewNote({
+                  ...newNote,
+                  text: NewVal,
+                });
+              }}
+              type="url"
+              placeholder="Enter note Url"
+            />
+          </FloatingLabel>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseAddModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={addNote}>
+            Create
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <div className="notes-list">
         {notesList.map((noteItem, index) => {
           return (
-            <Note note={noteItem} onNoteUpdate={updateNoteItem} key={index} />
+            <Note
+              note={noteItem}
+              onNoteUpdate={updateNoteItem}
+              onNoteDelete={deleteNoteItem}
+              key={index}
+            />
           );
         })}
       </div>
--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,15 +1,19 @@
-import { FC, FocusEvent } from "react"; // Importing Functional Component and it used to define type
+import { FC, FocusEvent, useState } from "react"; // Importing Functional Component and it used to define type
 import INote from "../../interfaces/note.interface";
 import "./Note.css";
 
 type Props = {
   note: INote;
   onNoteUpdate: (note: INote) => void;
+  onNoteDelete: (note: INote) => void;
   //   onNoteUpdate: Function;
 };
 
-const Note: FC<Props> = ({ note, onNoteUpdate }) => {
+const Note: FC<Props> = ({ note, onNoteUpdate, onNoteDelete }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
   const noteTextUpdated = (e: FocusEvent<HTMLDivElement>) => {
+    setIsFocused(false);
     console.log("note text change");
     const newTextValue = e.currentTarget.textContent;
     if (newTextValue === note.text) {
@@ -24,9 +28,20 @@ const Note: FC<Props> = ({ note, onNoteUpdate }) => {
   };
 
   return (
-    <div className="note">
+    <div className={isFocused ? "note note--focused" : "note"}>
+      <button
+        onClick={() => {
+          onNoteDelete(note);
+        }}
+        type="button"
+        className="btn-close"
+        aria-label="Close"
+      ></button>
       <div
         onBlur={noteTextUpdated}
+        onFocus={() => {
+          setIsFocused(true);
+        }}
         contentEditable={true}
         suppressContentEditableWarning={true}
         className="note__text"
